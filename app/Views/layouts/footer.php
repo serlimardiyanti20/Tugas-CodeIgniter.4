ef444374b36f7e7f7471de8e47c50e7a523ce75e
serlimardiyanti20 <serlimardiyanti@gmail.com>
2023-05-17 15:19:31 +0700

    percobaan commit

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer ">
+    <div class="container ">
+        <span class="text-muted ">Serli.</span>
+    </div>
+</footer>
---
+++ app/Views/layoutsV2/header.php
@@ -0,0 +1,3 @@
+<h1 class="h3 mb-2 text-gray-800">Toko Serli</h1>
+<p class="mb-4">Welocome
+    <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
